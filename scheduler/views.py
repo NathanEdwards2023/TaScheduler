c7ca02ecd248e59700a08c07906a1822cbc9d4a0
Rorr4 <73552535+Rorr4@users.noreply.github.com>
2024-04-15 18:52:45 -0500

    Update views.py
    Removed duplicate code, added stub method for createAccount.

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
-
 def home(request):
     return render(request, 'home.html')
 
 def courseManagement(request):
     return render(request, 'courseManagement.html')
+
+def createAccount(request):
+    #Stub method, complete later
+    return render(request, 'createAccount.html')
