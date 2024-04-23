5f87691892edd16f2870988156070b1ff0f2def3
NathanEdwards2023 <144078237+NathanEdwards2023@users.noreply.github.com>
2024-04-23 11:24:44 -0500

    Update tests.py
    Error with github. Merge before other commits.

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -4,6 +4,41 @@ from django.contrib.auth.models import User
 
 from adminAssignmentPage import AdminAssignmentPage
 from scheduler.models import UserTable, CourseTable, LabTable
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+
+# Acceptance Tests for Login
+class LoginTestCase(TestCase):
+    def setUp(self):
+        user = get_user_model()
+        self.user = user.objects.create_user(username='testuser', password='password')
+        self.login_url = reverse('login')
+        self.home_url = reverse('home')
+
+    def test_login_success(self):
+        response = self.client.post(self.login_url, {'username': 'testuser', 'password': 'password'}, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated)
+        self.assertRedirects(response, expected_url=reverse('home'))
+
+    def test_login_failure(self):
+        response = self.client.post(self.login_url, {'username': 'testuser', 'password': 'wrong'}, follow=True)
+        self.assertFalse(response.context['user'].is_authenticated)
+        self.assertTemplateUsed(response, 'registration/login.html')
+        self.assertContains(response, "Please enter a correct username and password.")
+
+    def test_login_redirect_to_home(self):
+        response = self.client.post(self.login_url, {'username': 'testuser', 'password': 'password'}, follow=True)
+        self.assertRedirects(response, self.home_url, status_code=302, target_status_code=200)
+        self.assertTrue(response.context['user'].is_authenticated)
+
+    def test_logout(self):
+        self.client.login(username='testuser', password='password')
+        logout_url = reverse('logout')
+        response = self.client.get(logout_url, follow=True)
+        self.assertFalse(response.context['user'].is_authenticated)
+        self.assertRedirects(response, expected_url=reverse('login'))
 
 
 class TestCreateCourse(unittest.TestCase):
