336f37073406b4c4fb1a95f774b7376c59f70b87
andy lien <andylien97@gmail.com>
2024-05-07 15:52:33 -0500

    fixing createLab tests (half working), also implemented CLS(half working)

--- scheduler/views.py
+++ scheduler/views.py
@@ -105,6 +105,28 @@ def courseManagement(request):
                     messages.error(request, message, extra_tags='lab_error')
                 return redirect('courseManagement')
 
+            if 'createLabBtn' in request.POST:
+                labSection = request.POST.get('labSection')
+                courseSelect = request.POST.get('courseSelect')
+
+                #try:
+                 #   success, message = admin_page.createLabSection(courseSelect, labSection)
+                  #  if success:
+                   #     messages.success(request, message)
+                    #else:
+                     #   messages.error(request, message)
+                #except ValueError as msg:
+                 #   messages.error(request, msg)
+                try:
+                    admin_page.createLabSection(courseSelect, labSection)
+                    return render(request, 'courseManagement.html',
+                                  {'courses': courses, 'TAs': TAs, 'instructors': instructors, 'labs': labs,
+                                   'joinEntries': joinEntries, 'createMessages': "Course successfully created"})
+                except ValueError as msg:
+                    return render(request, 'courseManagement.html',
+                                  {'courses': courses, 'TAs': TAs, 'instructors': instructors, 'labs': labs,
+                                   'joinEntries': joinEntries, 'createMessages': msg})
+
         return redirect('courseManagement')
 
 
